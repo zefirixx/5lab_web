d40677a7ff47b2e5fd5aa40c1bee9173165a27b5
зефирка3 <catsunsety@gmail.com>
2026-03-02 19:25:36 +0200

    Update Conference.php

--- lab/www/Conference.php
+++ lab/www/Conference.php
@@ -27,4 +27,14 @@ class Conference {
         $stmt = $this->pdo->query("SELECT * FROM conference_registrations ORDER BY id DESC");
         return $stmt->fetchAll();
     }
+
+    public function update($id, $name) {
+        $stmt = $this->pdo->prepare("UPDATE conference SET name=? WHERE id=?");
+        $stmt->execute([$name, $id]);
+    }
+
+    public function delete($id) {
+        $stmt = $this->pdo->prepare("DELETE FROM conference WHERE id=?");
+        $stmt->execute([$id]);
+    }
 }
